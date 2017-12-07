71baa2266a3e5d4a421eca80f431bb1e2d629ba6
Yuliang-Liu <510490360@qq.com>
2017-12-07 14:12:20 +0800

    update nms

--- lib/fast_rcnn/nms_wrapper.py
+++ lib/fast_rcnn/nms_wrapper.py
@@ -10,8 +10,6 @@ from nms.gpu_nms import gpu_nms
 from nms.cpu_nms import cpu_nms
 from nms.py_cpu_nms import py_cpu_pnms
 
-from nms.quad_gpu_nms import quad_gpu_nms
-
 def nms(dets, thresh, force_cpu=False):
     """Dispatch to either CPU or GPU NMS implementations."""
 
@@ -27,7 +25,4 @@ def pnms(dets, thresh):
 
     if dets.shape[0] == 0:
         return []
-    if cfg.USE_GPU_PNMS:
-        return quad_gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
-    else:
-        return py_cpu_pnms(dets, thresh)
+    return py_cpu_pnms(dets, thresh)
